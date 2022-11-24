9a12d255e6cfcc42471670988dbc6eca40fc9ca8
GiovanniTheo <112666306+GiovanniTheo@users.noreply.github.com>
2022-11-24 15:38:59 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-
+#abertura e importação do arquivo fasta
 fasta_filename = sys.argv[1]
 fasta_filehandle = open(fasta_filename, 'r') 
 
 seq_id = None
 seq_string = ''
-
+#criação das variáveis se_id e seq_string
 for line in fasta_filehandle:
     line = line.rstrip()
 
@@ -19,11 +19,11 @@ for line in fasta_filehandle:
                 print("{0}{1}".format(sep, seq_string[offset:offset+3]), end='')
                 sep=' '
             print()
-
+#imprime os sq e codons criados
         seq_def = line.lstrip('>').split(' ', maxsplit=1)
         seq_id = seq_def[0]
         seq_string = ''
-        
+        #ficca vazio de novo para uma próxima sequência
     else:
         seq_string += line.upper()
 
